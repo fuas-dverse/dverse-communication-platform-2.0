d29a17098d3af4670a5d2165c7126eb4adb82196
A.R. Mazilu <149481225+abelm21@users.noreply.github.com>
2026-04-16 16:04:45 +0200

    Revert "feat: chat-app experiment (FastAPI + React + Zenoh bots)"

--- experiments/zenoh-ping-pong/send_starting_message.py
+++ experiments/zenoh-ping-pong/send_starting_message.py
@@ -1,15 +1,5 @@
-import os
 import zenoh
 
-config_file = os.environ.get("CONFIG_FILE")
-
-if config_file:
-    print(f"using zenoh config from file: {config_file}")
-    config = zenoh.Config.from_file(config_file)
-else:
-    print("using default zenoh config")
-    config = zenoh.Config()
-
-with zenoh.open(config) as session:
+with zenoh.open(zenoh.Config()) as session:
     session.put("ping", "ping")
 
